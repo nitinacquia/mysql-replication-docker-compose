afa74c81600f5ad9d10eb804cdb18198b281a843
Nitin Nagpal <nitin.nagpal@acquia.com>
2017-05-02 14:56:28 +0530

    Initial Commit

---
+++ cleanup.sh
@@ -0,0 +1,13 @@
+echo "Stopping Containers..."
+docker stop $(docker ps -a -q)
+echo "Removing Containers..."
+docker rm $(docker ps -a -q)
+echo "Cleaning Directory Master"
+rm -rf master/backup/*
+rm -rf master/data/*
+rm -rf master/log/*
+echo "Cleaning Directory Slave"
+rm -rf slave/backup/*
+rm -rf slave/data/*
+rm -rf slave/log/*
+echo "Done"
